59f270ca3608f7aa28acc6aa480825f5c7de71b0
Shin Ishimaru <shinishimaru@gmail.com>
2015-05-13 19:27:34 +0900

    first commit

---
+++ Assets/UICallBack.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UICallBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void ClickTest(){
+		Debug.Log("Clicked.");
+	}
+	public void ChangeSlider(float s){
+		Debug.Log(s);
+	}
+	
+	public void ChangeToggleTokyo(bool b){
+		if(b){
+			Debug.Log("Tokyo On");
+		}else{
+			Debug.Log("Tokyo Off");
+		}
+	}
+
+	public void ChangeToggleOsaka(bool b){
+		if(b){
+			Debug.Log("Osaka On");
+		}else{
+			Debug.Log("Osaka Off");
+		}
+	}
+
+}
